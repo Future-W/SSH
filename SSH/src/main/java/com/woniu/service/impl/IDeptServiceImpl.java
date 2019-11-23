7ca8195bf5cf704e1f2c808abf49b75f10e281e5
wym <510395053@qq.com>
2019-11-23 17:18:41 +0800

    删除hibernate主，子配置文件

--- SSH/src/main/java/com/woniu/service/impl/IDeptServiceImpl.java
+++ SSH/src/main/java/com/woniu/service/impl/IDeptServiceImpl.java
@@ -2,18 +2,23 @@ package com.woniu.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.woniu.dao.IDeptDAO;
 import com.woniu.pojo.Dept;
 import com.woniu.service.IDeptService;
 @Service
 @Transactional
 public class IDeptServiceImpl implements IDeptService {
 
+	@Autowired
+	private IDeptDAO dd;
+	
 	@Override
 	public void save(Dept dept) {
-		// TODO Auto-generated method stub
+		dd.save(dept);
 
 	}
 
